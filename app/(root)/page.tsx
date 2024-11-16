4584d07fc03b091c9735f82522bcb65f449be38f
Graeme Byrne <grmbrn89@gmail.com>
2024-11-16 12:55:39 +0100

    fetch data, type safety and cache live api

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,15 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams}: {
   searchParams: Promise<{query?: string}>
 }) {
   const query = (await searchParams).query
+  const params = {search: query || null}
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params})
 
   return (
     <>
@@ -28,7 +29,7 @@ export default async function Home({searchParams}: {
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard, index: number) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
@@ -36,6 +37,8 @@ export default async function Home({searchParams}: {
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
